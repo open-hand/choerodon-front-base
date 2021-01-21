d58fcbf1a9f1aaa8d52a8ef1637c9e27c96eb50b
NollieLeo <736653759@qq.com>
2021-01-21 15:06:04 +0800

    [FIX] 修复组织名称校验规则

--- react/src/app/iam/containers/organization/organization-setting/stores/OrganizationDataSet.js
+++ react/src/app/iam/containers/organization/organization-setting/stores/OrganizationDataSet.js
@@ -1,5 +1,13 @@
 import pick from 'lodash/pick';
 
+function validateName(value, name, record) {
+  const patternHTMLEmpty = /^[-—\.\w\s\u4e00-\u9fa5]{1,32}$/;
+  if (!value || !patternHTMLEmpty.test(value)) {
+    return '组织名称只能由汉字、字母、数字、"_"、"."、"-"、"——"和空格组成';
+  }
+  return true;
+}
+
 export default ({ id = 0 }) => ({
   // autoCreate: true,
   autoQuery: true,
@@ -36,10 +44,16 @@ export default ({ id = 0 }) => ({
   },
   fields: [
     { name: 'imageUrl', type: 'string', label: '组织Logo' },
-    { name: 'tenantName', type: 'string', label: '组织名称', defaultValue: '汉得', required: true },
-    { name: 'tenantNum', type: 'string', label: '组织编码', required: true },
+    {
+      name: 'tenantName', type: 'string', label: '组织名称', defaultValue: '汉得', required: true, validator: validateName,
+    },
+    {
+      name: 'tenantNum', type: 'string', label: '组织编码', required: true,
+    },
     { name: 'address', type: 'string', label: '组织所在地' },
-    { name: 'homePage', type: 'url', label: '官网地址', defaultValue: '无' },
+    {
+      name: 'homePage', type: 'url', label: '官网地址', defaultValue: '无',
+    },
     { name: 'ownerRealName', type: 'string', label: '所有者' },
   ],
 });
