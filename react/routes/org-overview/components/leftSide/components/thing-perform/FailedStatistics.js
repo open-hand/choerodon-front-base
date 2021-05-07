467c3f7a6b7f33acaad1664a830640793f71ebfc
20399 <yanfang.lin@hand-china.com>
2021-05-07 17:08:39 +0800

    [IMP] 修改概览页面跳转到事务管理参数问题

--- react/routes/org-overview/components/leftSide/components/thing-perform/FailedStatistics.js
+++ react/routes/org-overview/components/leftSide/components/thing-perform/FailedStatistics.js
@@ -22,7 +22,7 @@ const FailedStatistics = withRouter(observer((props) => {
   } = useFailedStatisticsStore();
 
   const handleClickSagaRecord = (record) => {
-    history.push(`/asgard/org-saga-instance${search}&sagaCode=${record.get('sagaCode')}-${record.get('id')}`);
+    history.push(`/asgard/org-saga-instance${search}&sagaCode=${record.get('sagaCode')}-${record.get('searchId')}`);
   };
 
   const renderSagaCode = ({ value, record }) => (
