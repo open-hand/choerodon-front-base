b2d8d8ecdec64b4a8181cbb88ff09ca93198f3d0
isaac <273070111@qq.com>
2021-10-22 17:08:47 +0800

    [ADD]增加团队成员功能

--- react/routes/project-user/list/stores/filterDataSet.ts
+++ react/routes/project-user/list/stores/filterDataSet.ts
@@ -5,15 +5,47 @@
  * i made my own lucky
  */
 
+import { DataSet } from 'choerodon-ui/pro';
 import { FieldProps, FieldType } from '@/interface';
+import { rolesApiConfig } from '@/api';
 
 const mapping: {
  [key: string]: FieldProps
 } = {
-  username: {
-    name: 'username',
-    label: '用户名',
+  params: {
+    name: 'params',
     type: 'string' as FieldType,
+  },
+  role: {
+    name: 'roles',
+    label: '角色',
+    type: 'string' as FieldType,
+    textField: 'name',
+    valueField: 'id',
+    options: new DataSet({
+      autoQuery: true,
+      transport: {
+        read: {
+          ...rolesApiConfig.getRoles(true),
+        },
+      },
+    }),
+  },
+  status: {
+    name: 'enabled',
+    label: '状态',
+    type: 'boolean' as FieldType,
+    textField: 'name',
+    valueField: 'value',
+    options: new DataSet({
+      data: [{
+        name: '启用',
+        value: true,
+      }, {
+        name: '停用',
+        value: false,
+      }],
+    }),
   },
 };
 const Index = () => ({
