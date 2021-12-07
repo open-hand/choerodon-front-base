d33edf3e0e910f3e3bd5baa3d5f3b415ab760275
翁恺敏 <736653759@qq.com>
2021-12-07 13:56:22 +0800

    [FIX] fix locale

--- react/locale/en_US/client.js
+++ react/locale/en_US/client.js
@@ -1,9 +1,9 @@
 const client = {
   'c7ncd.client.clientID': 'Client ID',
   'c7ncd.client.edit': 'Edit',
-  'c7ncd.client.add': 'Add',
+  'c7ncd.client.add': 'Create',
   'c7ncd.client.delete': 'Delete',
-  'c7ncd.client.license': 'License',
+  'c7ncd.client.license': 'Type of authorization',
   'c7ncd.client.addtext': 'Add',
 };
 
