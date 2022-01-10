29ad20cacd6f8f30e8c1fd46f6d21cb1c64cb160
maox <xm1440606925@126.com>
2022-01-10 13:56:21 +0800

    [FIX]平台层hzero跳转多语言修改

--- react/locale/en_US/hzero-page.js
+++ react/locale/en_US/hzero-page.js
@@ -1,5 +1,5 @@
 const hzeroPage = {
-  'c7n.hzero.page.link': 'Skip',
+  'c7n.hzero.page.link': 'Jump to the Hzero framework',
   'c7n.hzero.page.user.describe': 'This page has been replaced with HZERO sub-account management page. Please click the button below to jump to HZERO framework operation',
   'c7n.hzero.page.role.describe': 'This page has been replaced with the HZERO Role Management page. Please click the button below to go to the HZERO framework operation',
   'c7n.hzero.page.menu.describe': 'This page has been replaced with the HZERO menu management page. Please click the button below to jump to the HZERO framework operation',
