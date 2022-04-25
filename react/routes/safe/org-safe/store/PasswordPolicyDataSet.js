e8ada7cf331eddf824734931ca22f51420f49586
jamin <245139263@qq.com>
2022-04-25 14:15:35 +0800

    [FIX]-组织层安全ui

--- react/routes/safe/org-safe/store/PasswordPolicyDataSet.js
+++ react/routes/safe/org-safe/store/PasswordPolicyDataSet.js
@@ -169,7 +169,7 @@ export default function passwordPoliciesDataSet(organizationId, formatCommon, fo
     },
     {
       name: 'userLoginSecondCheck',
-      label: 'xxx',
+      label: '',
     },
   ];
 
